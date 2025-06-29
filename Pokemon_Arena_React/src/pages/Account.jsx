91ce4fd0f1bc6fb4a8551b5219f7def2fd008a0a
KrzysztofZdebski <km.zdebski@gmail.com>
2025-06-29 14:09:21 +0200

    a

--- Pokemon_Arena_React/src/pages/Account.jsx
+++ Pokemon_Arena_React/src/pages/Account.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import Logout from "../components/Logout";
 import Ranking from "../components/Ranking";
+import AuthContext from "../utils/authProvider";
 
 function Account() {
     const {username, email} = useContext(AuthContext);
@@ -8,6 +9,11 @@ function Account() {
     return (
         <div className="flex items-center justify-center w-screen min-h-screen bg-striped-yellow">
             <div className="flex flex-col items-center justify-center w-full max-w-md px-6 py-10 border-4 shadow-2xl bg-menu-blue bg-opacity-90 rounded-2xl border-menu-yellow">
+                <h1
+                  className="mb-6 text-4xl font-bold text-pokemon-yellow font-[PokemonFont] tracking-wider text-center drop-shadow-[0_0_5px_rgba(255,255,255,0.75)]"
+                  style={{ fontFamily: "PokemonFont, monospace" }}>
+                    `Witaj, {username}!, email: {email}`
+                  </h1>
                 <h2
                   className="mb-8 text-3xl md:text-4xl font-bold text-pokemon-yellow font-[PokemonFont] tracking-wider text-center drop-shadow-[0_0_5px_rgba(255,255,255,0.75)]"
                   style={{ fontFamily: "PokemonFont, monospace" }}
